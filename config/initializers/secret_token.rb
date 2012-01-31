203bd719b13661827e2f41cad4b401df42470406
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-01-31 19:07:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timetablesonrails::Application.config.secret_token = 'b02677f16b29a3563a0e59d4af54a63ad045f5de298310ea3eb495d708b430ed23749dd590316768a2b27ea6f7bd928c5efc7e71c231fa2f6d22335753ffe4af'
