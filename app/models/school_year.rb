df79c7d37ec92e1c3dd1c3590780ee8dabe146c6
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-02-26 16:31:15 +0100

    Add SchoolYear class

---
+++ app/models/school_year.rb
@@ -0,0 +1,41 @@
+require 'date'
+require_relative './semester'
+
+class SchoolYear
+  attr_accessor :start, :end
+
+  def initialize(start_date=Date.new(Time.now.year), end_date=start_date)
+    @archived = false
+    @activated = false
+    @semesters = []
+    @start = start_date
+    @end = end_date
+  end
+
+  def archive!
+    @archived = true
+  end
+
+  def archived?
+    @archived
+  end
+
+  def activate!
+    @archived = false
+    @activated = true
+  end
+
+  def activated?
+    @activated
+  end
+
+  def semesters
+    @semesters
+  end
+
+  def new_semester(name, start_date, end_date)
+    semester = Semester.new(name, start_date, end_date)
+    semester.school_year = self
+    @semesters << semester
+  end
+end
---
+++ spec/models/school_year_spec.rb
@@ -0,0 +1,73 @@
+require_relative '../spec_helper_lite.rb'
+require_relative '../../app/models/school_year'
+
+describe SchoolYear do
+  before do
+    @date = Time.now
+  end
+
+  subject { SchoolYear.new }
+
+  it "supports reading and writing a starting date" do
+    subject.start = @date
+    subject.start.must_equal @date
+  end
+
+  it "supports reading and writing an ending date" do
+    subject.end = @date
+    subject.end.must_equal @date
+  end
+
+  it "is not archived when newly created" do
+    refute subject.archived? 
+  end
+
+  it "is not activated when newly created" do
+    refute subject.activated?
+  end
+
+  it "can be archived" do
+    subject.archive!
+    assert subject.archived?
+  end
+
+  it "can be activated" do
+    subject.activate!
+    assert subject.activated?
+  end
+
+  it "is unarchived when activated" do
+    subject.archive!
+    assert subject.archived?
+    subject.activate!
+    refute subject.archived?
+  end
+
+  it "has semesters" do
+    subject.semesters.must_be_empty 
+  end
+
+  it "supports being initialized with attributes" do
+    school_year = SchoolYear.new(@date, @date + 10)
+    school_year.start.must_equal @date
+    school_year.end.must_equal @date + 10
+  end
+
+  describe "#add_semester" do
+
+    it "creates a semester" do
+      subject.new_semester "Semester", @date, @date
+      semester = subject.semesters.first
+      semester.name.must_equal "Semester"
+      semester.start.must_equal @date
+      semester.end.must_equal @date
+    end
+
+    it "sets the semester's school year reference to itself" do
+      subject.new_semester "Semester", @date, @date
+      subject.semesters.first.school_year.must_equal subject
+    end
+
+  end
+
+end
