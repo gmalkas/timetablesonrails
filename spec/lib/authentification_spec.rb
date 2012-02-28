ddc00a552784102e54335ebae59cf71b25e905db
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-02-28 19:06:05 +0100

    Add Authentification class

--- spec/lib/authentification_spec.rb
+++ spec/lib/authentification_spec.rb
@@ -1,12 +1,21 @@
-require_relative '../spec_helper_lite.rb'
-require_relative '../../lib/authentification.rb'
+require_relative '../spec_helper_lite'
+require_relative '../../lib/authentification'
+require_relative '../../lib/password_hasher'
+
+require 'ostruct'
 
 module TimetablesOnRails
   describe Authentification do
 
-    it "can authentificate a user with a username and a password" do
+    it "can authentificate a resource with a login and a password" do
+      password = 'littlebee'
+      bad_password = 'bigbee'
+      password_hash = PasswordHasher.hash password
+      resource = OpenStruct.new password_hash: password_hash
 
+      assert Authentification.authenticate resource, :password_hash, password
+      refute Authentification.authenticate resource, :password_hash, bad_password
     end
-
+    
   end
 end
