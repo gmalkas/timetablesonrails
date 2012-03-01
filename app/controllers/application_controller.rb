134bd6b758d820d99f16b5d71b6fdd26d93010ee
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-03-01 18:04:43 +0100

    Add current_user method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
+require 'timetablesonrails/user_session'
+
 class ApplicationController < ActionController::Base
+
+  include TimetablesOnRails
+
   protect_from_forgery
+
+  def current_user
+    @user_session ||= UserSession.new session, cookies
+    @user_session.current_user
+  end
+
+  helper_method :current_user
 end
