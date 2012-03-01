3a1897a0f7e951a2b85163cf45e3096773356c55
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-03-01 22:31:29 +0100

    Clean up UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,51 @@
 class HomeController < ApplicationController
   def index
     if current_user
+		laurence = {:name => "L. Rozé"}
+		marin = {:name => "M. Bertier"}
+		yvan = {:name => "Y. Leplumey"}
+		daniele = {:name => "D. Quichaud"}
+		maud = {:name => "M. Marchal"}
+    yann = {:name => "Y. Ricquebourg"}
+		abdelly = {:name => "A. Leguesdron"}
+    eric = {:name => "E. Anquetil"}
+		edouard = {:name => "E. Monnier"}
+
+		sem5 = {
+			:id => '5',
+			:courses => [
+				{:name => 'Assembleur', :postulants => [laurence]},
+				{:name => 'Logique séquentielle', :postulants => [marin, yvan]},
+				{:name => 'Langages et grammaires', :postulants => [daniele]},
+				{:name => 'Modélisation', :postulants => [maud]},
+				{:name => 'Structures de données', :postulants => [yann]},
+				{:name => 'Modèles stochastiques', :postulants => []}
+		  ]
+    }
+
+		sem6 = { 
+			:id => '6',
+			:courses => [
+				{:name => 'Base de données', :postulants => []},
+				{:name => 'Ruby on Rails', :postulants => [marin, daniele, yann, yvan]},
+				{:name => 'Architecture des systèmes', :postulants => [daniele]},
+				{:name => 'Modélisation', :postulants => []},
+				{:name => 'Structure de données', :postulants => [yann]},
+				{:name => 'Gestion du risque II', :postulants => [yvan, marin]}
+			]
+    }
+
+		sem7 = { :id => '7', :courses => [] }
+		sem8 = {
+      :id => '8',
+			:courses => [
+        {:name => 'Programmation en logique', :postulants => [edouard]},
+				{:name => 'Prog. Orientée objets', :postulants => [eric, abdelly]},
+				{:name => 'Systèmes', :postulants => [eric, marin]}
+			]
+		}
+
+		@semesters = [sem5, sem6, sem7, sem8]
       render 'dashboard'
     else
       render 'index', :layout => 'sign_in'
@@ -9,6 +54,11 @@ class HomeController < ApplicationController
   end
 
   def sign_in
+    if current_user
+      redirect_to root_path
+      return
+    end
+
     user = User.find_by_username params[:username]
     if user && TimetablesOnRails::UserAuthentification.authenticate(user, params[:password])
       permanent = params[:remember_me] == '1'
@@ -17,7 +67,13 @@ class HomeController < ApplicationController
       redirect_to root_path
     else
       @sign_in_error = "Nom d'utilisateur ou mot de passe incorrect"
-      render 'index'
+      render 'index', :layout => 'sign_in'
     end
+  end
+
+  def sign_out
+    user_session = TimetablesOnRails::UserSession.new session, cookies
+    user_session.destroy
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Timetablesonrails::Application.routes.draw do
   get 'administration' => 'administration#dashboard', as: 'dashboard'
   post '/' => 'home#sign_in', as: 'sign_in'
+  get '/deconnexion' => 'home#sign_out', as: 'sign_out'
 
   root :to => 'home#index'
 end
