64b921a7cbffe4080e50753698da98c61fbcf845
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-03-01 20:23:59 +0100

    Add session and error management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,26 @@
-require 'timetablesonrails/user_session'
-
 class ApplicationController < ActionController::Base
-
-  include TimetablesOnRails
 
   protect_from_forgery
 
+	rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+	rescue_from Errno::ECONNREFUSED, :with => :unavailable
+
+	private
+
+  def not_found
+    render "errors/404", :status => 404
+  end
+
+	def unauthorized
+    render "errors/401", :status => 401
+	end
+
+	def unavailable
+    render "errors/503", :status => 503 
+	end
+
   def current_user
-    @user_session ||= UserSession.new session, cookies
+    @user_session ||= TimetablesOnRails::UserSession.new session, cookies
     @user_session.current_user
   end
 
