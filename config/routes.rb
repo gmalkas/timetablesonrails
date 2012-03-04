2c1456019e5dbc09cfc8c4f0cfb6ebed3308786c
Richard Lagrange <richardlagrange@Richard-Lagranges-MacBook-Pro.local>
2012-03-04 15:34:31 +0100

    Add route to courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Timetablesonrails::Application.routes.draw do
   get 'administration' => 'administration#dashboard', as: 'dashboard'
   get 'home/index'
+  get 'courses' => 'school_years#index'
 
   root :to => 'home#index'
 end
