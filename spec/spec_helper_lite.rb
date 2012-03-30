2cdac74e3e072e363b921b793d691a788d482356
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-03-30 20:29:37 +0200

    Load only ActiveRecord for unit tests

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -1,1 +1,16 @@
-require_relative './spec_helper.rb'
+require 'active_record'
+require 'rspec/rails/extensions/active_record/base'
+
+ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+
+schema_path = File.expand_path('../db/schema.rb', File.dirname(__FILE__))
+load schema_path
+
+RSpec.configure do |config|
+  config.around do |example|
+    ActiveRecord::Base.transaction do
+      example.run
+      raise ActiveRecord::Rollback
+    end
+  end
+end
