72b07a5e24c1efe45f0aa99d570cc6a1f552a9bb
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-02 19:09:32 +0200

    Add tests to fully cover Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,18 @@
+require_relative './user'
+require_relative './course'
+
 class Ability
   include CanCan::Ability
 
   def initialize(user)
     user ||= User.new
+    return if user.new_record?
+
     if user.administrator?
       can :manage, :all
       cannot [:apply, :withdraw], Course
     else
-      can :read, Course
-      can [:apply, :withdraw], Course
+      can [:read, :apply, :withdraw], Course
     end
   end
 end
---
+++ spec/models/ability_spec.rb
@@ -0,0 +1,62 @@
+require 'cancan/matchers'
+
+require_relative '../spec_helper'
+require_relative '../../app/models/ability'
+
+describe User do
+  describe "abilities" do
+    subject { ability }
+    let(:ability) { Ability.new user }
+
+    context "when is a guest" do
+      let(:user) { User.new }
+
+      it "cannot do anything" do
+        ability.should_not be_able_to :manage, :all
+      end
+    end
+
+    context "when is a teacher" do
+      let(:user) {
+        user = User.new username: "gmalkas", firstname: "Gabriel", lastname: "Malkas"
+        user.password = "gabriel"
+        user.save
+        user
+      }
+
+      it "can read courses" do
+        ability.should be_able_to :read, Course.new
+      end
+
+      it "can apply and withdraw from courses management" do
+        ability.should be_able_to :apply, Course.new
+        ability.should be_able_to :withdraw, Course.new
+      end
+
+      it "cannot manage courses" do
+        ability.should_not be_able_to :update, Course.new
+        ability.should_not be_able_to :destroy, Course.new
+      end
+    end
+
+    context "when is an administrator" do
+      let(:user) {
+        user = User.new username: "gmalkas", firstname: "Gabriel", lastname: "Malkas"
+        user.password = "gabriel"
+        user.administrator = true
+        user.save
+        user
+      }
+
+      it "can do almost everything" do
+        ability.should be_able_to :manage, :all
+      end
+
+      it "cannot apply nor withdraw from courses management" do
+        ability.should_not be_able_to :apply, Course.new
+        ability.should_not be_able_to :withdraw, Course.new
+      end
+    end
+    
+  end
+end
