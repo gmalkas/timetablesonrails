34ae18c2d36818e38cfef416ab7f6eea3a00bc4e
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-06 13:10:56 +0200

    Fix the issue that prevented the usage of spec_helper_lite when saving users. SQLite Adapter needed an instance of Logger

--- spec/spec_helper_lite.rb
+++ spec/spec_helper_lite.rb
@@ -1,7 +1,11 @@
+ENV["RAILS_ENV"] ||= 'test'
+
 require 'active_record'
+require 'logger'
 require 'rspec/rails/extensions/active_record/base'
 
 ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
+ActiveRecord::Base.logger = Logger.new('/dev/null')
 
 schema_path = File.expand_path('../db/schema.rb', File.dirname(__FILE__))
 load schema_path
