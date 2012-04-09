8fb6169f34b6b5000e6d6293381b9d29b13a8f4c
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-04-09 18:08:35 +0200

    Remove notification examples from the dashboard view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   def dashboard
     @active_school_year = SchoolYearManager.instance.active_school_year
     if current_user.administrator?
-      @notifications = Notification.all.map {|n| NotificationPresenter.new n}
+      @notifications = TimetablesOnRails::DateRegroup.group_by_day(Notification.all.map {|n| NotificationPresenter.new n})
       render 'administrator_dashboard'
     else
       render 'teacher_dashboard'
