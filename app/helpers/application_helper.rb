dad72ed8592ad4372d1a32ac14f95f4afbc0aa76
Gabriel Malkas <gabriel.malkas@gmail.com>
2012-05-18 14:29:15 +0200

    Add comments for helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
+##
+#
+# = ApplicationHelper
+#
+# Defines a helper to handle titles.
+#
 module ApplicationHelper
+  
+  ##
+  # 
+  # Defines the current page's title.
+  #
 	def title(*titles)
     content_for(:title) { h titles.join(" - ") }
 	end
